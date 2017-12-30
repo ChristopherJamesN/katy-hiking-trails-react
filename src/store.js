d270adaa0c3a29d32be9c3a663b29f8805f8bc14
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 12:35:59 -0600

    Add localStorage and store files and update index accordingly

---
+++ src/store.js
@@ -0,0 +1,31 @@
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import thunk from 'redux-thunk';
+import loading from './reducers/loading';
+import { loadState, saveState } from './localStorage';
+
+const reducers = combineReducers({
+  userReducer,
+  loading,
+  notesReducer,
+  newsReducer
+});
+
+const persistedState = loadState();
+
+const middleware = [thunk];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  persistedState,
+  composeEnhancers(
+    applyMiddleware(...middleware)
+  )
+)
+
+store.subscribe(() => {
+  saveState({
+  });
+});
+export default store;
