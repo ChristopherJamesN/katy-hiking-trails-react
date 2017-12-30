86e31c7fdcf5bf6d153b81281ee9b5477f8fbf58
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 12:43:15 -0600

    Add loading reducer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
+import loading from './reducers/loading';
 import { loadState, saveState } from './localStorage';
+
+const reducers = combineReducers({
+  loading,
+});
 
 const persistedState = loadState();
 
@@ -9,6 +14,7 @@ const middleware = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
+  reducers,
   persistedState,
   composeEnhancers(
     applyMiddleware(...middleware)
