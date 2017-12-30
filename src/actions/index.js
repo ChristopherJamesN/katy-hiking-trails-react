347760268bd35f750e4f9f6e3bca4d631f9931d6
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 14:13:59 -0600

    Add a proxy URL to get around CORS issues.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 export function getTrails() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_TRAILS' })
-    return fetch('https://katy-hiking-trails.herokuapp.com/trails', {
-    method: "get", headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }})
+    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
+      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails.json'
+    return fetch(proxyUrl + targetUrl, {
+    method: "get", headers: { "Content-Type": "application/json" }})
         .then(response => {
           return response.json()
         }).then(payload => dispatch({ type: 'SHOW_TRAILS', payload }));
