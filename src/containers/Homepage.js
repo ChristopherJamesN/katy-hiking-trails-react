ad57b10f41a79f4cb9b45b6680eeb106383abfcc
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 17:45:43 -0600

    Update information on homepage.

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -4,7 +4,8 @@ class Homepage extends Component {
     render() {
       return(
         <div>
-          <p>Hello!</p>
+          <h1>Welcome to Katy Hiking Trails</h1>
+          <h3>Your guide to outdoor exploration in Katy, Texas</h3>
         </div>
       )}
   }
