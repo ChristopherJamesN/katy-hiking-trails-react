4883c215d027d3e668398ca55f67ca652da179f1
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 12:41:17 -0600

    Add all needed dependecies to package.json

--- src/store.js
+++ src/store.js
@@ -1,14 +1,6 @@
-import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import loading from './reducers/loading';
 import { loadState, saveState } from './localStorage';
-
-const reducers = combineReducers({
-  userReducer,
-  loading,
-  notesReducer,
-  newsReducer
-});
 
 const persistedState = loadState();
 
@@ -17,7 +9,6 @@ const middleware = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducers,
   persistedState,
   composeEnhancers(
     applyMiddleware(...middleware)
