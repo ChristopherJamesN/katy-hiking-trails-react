2a4ed95cf653b23964725dac2528d68dcbbfe99d
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-30 19:30:57 -0600

    Add alternate route if no link found

--- src/containers/TrailsShow.js
+++ src/containers/TrailsShow.js
@@ -29,7 +29,7 @@ class TrailsShow extends Component {
             <ListGroupItem><p>{this.props.trail.description}</p></ListGroupItem>
         </ListGroup>
         <ListGroup>
-            <Link key={this.props.trail.id} className="list-group-item list-group-item-action" to={this.props.trail.maplink} target="_blank">Link to trail map</Link>
+            <Link key={this.props.trail.id} className="list-group-item list-group-item-action" to={this.props.trail.maplink || "/"} target="_blank">Link to trail map</Link>
         </ListGroup>
         <br></br>
         <iframe src={'https://www.google.com/maps/embed/v1/place?key=' + process.env.REACT_APP_GOOGLE_EMBEDDED_MAPS_API + '&q=' + this.props.trail.name + ',Houston+TX'} width="100%" height="400" frameBorder="1" title="EmbeddedGoogleMap"></iframe>
