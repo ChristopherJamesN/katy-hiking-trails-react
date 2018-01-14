fc29563efd76df1fbd0624515da056f2f50fb603
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-13 22:23:16 -0600

    Add a get favorite trails action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
+export function getFavoriteTrails() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_FAVORITE_TRAILS' })
+    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
+      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails.json'
+    return fetch(proxyUrl + targetUrl, {
+    method: "get", headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }})
+        .then(response => {
+          return response.json()
+        }).then(payload => dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload }));
+  };
+}
+
 export function getTrails() {
   return (dispatch) => {
     dispatch({ type: 'LOADING_TRAILS' })
