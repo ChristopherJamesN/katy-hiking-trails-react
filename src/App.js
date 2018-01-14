a61840eb579f388714c8117418a104e4e383170e
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-13 22:10:15 -0600

    Add routes to sign in, sign up, and sign out forms to app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navigation from './containers/Navigation';
 import TrailsPage from './containers/TrailsPage';
 import BarkerTrailsPage from './containers/BarkerTrailsPage';
 import AddicksTrailsPage from './containers/AddicksTrailsPage';
+import SignOutLink from './components/auth/SignOutLink';
+import SignInForm from './components/auth/SignInForm';
+import SignUpForm from './components/auth/SignUpForm';
 import './App.css';
 
 class App extends Component {
@@ -18,6 +21,9 @@ class App extends Component {
               <Route exact path = "/" component = { Homepage } />
               <Route path = "/addicks" component = { AddicksTrailsPage } />
               <Route path = "/barker" component = { BarkerTrailsPage } />
+              <Route path="/signout" component={SignOutLink} />
+              <Route path="/signin" component={SignInForm} />
+              <Route path="/signup" component={SignUpForm} />
             </div>
         </div>
       </Router>
