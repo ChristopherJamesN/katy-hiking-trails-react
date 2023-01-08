484af1e569c5d4d4d87e6fcd7a95baff92ace668
ChristopherJamesN <nady.christopher@gmail.com>
2023-01-07 20:06:27 -0600

    Switch fetch urls to use https://katy-hiking-trails.fly.dev
    Previously the project was using the heroku urls for the project but
    it has been moved to https://katy-hiking-trails.fly.dev.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,212 +1,268 @@
 export function getFavoriteTrails() {
   return (dispatch) => {
-    dispatch({ type: 'LOADING_FAVORITE_TRAILS' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails/favorites.json'
+    dispatch({ type: 'LOADING_FAVORITE_TRAILS' });
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/trails/favorites.json';
     return fetch(proxyUrl + targetUrl, {
-    method: "get", headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }})
-        .then(response => {
-          return response.json()
-        }).then(payload => dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload }));
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload }));
   };
 }
 
 export function persistTrail(name, description, link, comments) {
   const noteInfo = JSON.stringify({
-    note:{
+    note: {
       name: name,
       description: description,
       link: link,
-      comments: comments
-    }
+      comments: comments,
+    },
   });
   return (dispatch) => {
-    dispatch({ type: 'SAVING_TRAIL' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails/favorites'
+    dispatch({ type: 'SAVING_TRAIL' });
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/trails/favorites';
     return fetch(proxyUrl + targetUrl, {
-      method: "post", body: noteInfo, headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }})
-      .then(response => response.json()).then(payload => dispatch({ type: 'ADD_TRAIL', payload }));
-  }
+      method: 'post',
+      body: noteInfo,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
+    })
+      .then((response) => response.json())
+      .then((payload) => dispatch({ type: 'ADD_TRAIL', payload }));
+  };
 }
 
 export function updateTRAIL(trailId, name, description, link, comments) {
   return (dispatch) => {
-    dispatch({ type: 'SAVING_TRAIL' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = `https://katy-hiking-trails.herokuapp.com/trails/favorites/${trailId}`
+    dispatch({ type: 'SAVING_TRAIL' });
+    var proxyUrl = '',
+      targetUrl = `https://katy-hiking-trails.fly.dev/trails/favorites/${trailId}`;
     return fetch(proxyUrl + targetUrl, {
-      method: "put", body: JSON.stringify({note:{
-        name: name,
-        description: description,
-        link: link,
-        comments: comments
-      }
-    }), headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }
+      method: 'put',
+      body: JSON.stringify({
+        note: {
+          name: name,
+          description: description,
+          link: link,
+          comments: comments,
+        },
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
     })
-      .then(response => response.json()).then(payload => dispatch({ type: 'SAVING_NOTE' }))
-      .then(payload => {
-      dispatch({ type: 'LOADING_FAVORITE_TRAILS' })
-      return fetch(proxyUrl + 'https://katy-hiking-trails.herokuapp.com/trails/favorites', {
-      method: "get", headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }})
-          .then(response => {
-            return response.json()
-          }).then(payload => dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload }))
+      .then((response) => response.json())
+      .then((payload) => dispatch({ type: 'SAVING_NOTE' }))
+      .then((payload) => {
+        dispatch({ type: 'LOADING_FAVORITE_TRAILS' });
+        return fetch(
+          proxyUrl + 'https://katy-hiking-trails.fly.dev/trails/favorites',
+          {
+            method: 'get',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+            },
+          }
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((payload) =>
+            dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload })
+          );
       });
-  }
+  };
 }
 
 export function deleteTrail(trailId) {
   return (dispatch) => {
-    dispatch({ type: 'DELETING_TRAIL' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = `https://katy-hiking-trails.herokuapp.com/trails/favorites/${trailId}`
+    dispatch({ type: 'DELETING_TRAIL' });
+    var proxyUrl = '',
+      targetUrl = `https://katy-hiking-trails.fly.dev/trails/favorites/${trailId}`;
     return fetch(proxyUrl + targetUrl, {
-      method: "delete",
-       headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }
-    })
-    .then(payload => {
-    dispatch({ type: 'LOADING_FAVORITE_TRAILS' })
-    return fetch(proxyUrl + 'https://katy-hiking-trails.herokuapp.com/trails/favorites', {
-    method: "get", headers: { "Content-Type": "application/json", 'Authorization': 'Bearer ' + localStorage.getItem('jwt') }})
-        .then(response => {
-          return response.json()
-        }).then(payload => dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload }))
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
+    }).then((payload) => {
+      dispatch({ type: 'LOADING_FAVORITE_TRAILS' });
+      return fetch(
+        proxyUrl + 'https://katy-hiking-trails.fly.dev/trails/favorites',
+        {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+          },
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((payload) => dispatch({ type: 'SHOW_FAVORITE_TRAILS', payload }));
     });
-  }
+  };
 }
 
 export function getTrails() {
   return (dispatch) => {
-    dispatch({ type: 'LOADING_TRAILS' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails.json'
+    dispatch({ type: 'LOADING_TRAILS' });
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/trails.json';
     return fetch(proxyUrl + targetUrl, {
-    method: "get", headers: { "Content-Type": "application/json" }})
-        .then(response => {
-          return response.json()
-        }).then(payload => dispatch({ type: 'SHOW_TRAILS', payload }));
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => dispatch({ type: 'SHOW_TRAILS', payload }));
   };
 }
 
 export function getAddicksTrails() {
   return (dispatch) => {
-    dispatch({ type: 'LOADING_TRAILS' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails/addicks.json'
+    dispatch({ type: 'LOADING_TRAILS' });
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/trails/addicks.json';
     return fetch(proxyUrl + targetUrl, {
-    method: "get", headers: { "Content-Type": "application/json" }})
-        .then(response => {
-          return response.json()
-        }).then(payload => dispatch({ type: 'SHOW_TRAILS', payload }));
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => dispatch({ type: 'SHOW_TRAILS', payload }));
   };
 }
 
 export function getBarkerTrails() {
   return (dispatch) => {
-    dispatch({ type: 'LOADING_TRAILS' })
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/trails/barker.json'
+    dispatch({ type: 'LOADING_TRAILS' });
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/trails/barker.json';
     return fetch(proxyUrl + targetUrl, {
-    method: "get", headers: { "Content-Type": "application/json" }})
-        .then(response => {
-          return response.json()
-        }).then(payload => dispatch({ type: 'SHOW_TRAILS', payload }));
+      method: 'get',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => dispatch({ type: 'SHOW_TRAILS', payload }));
   };
 }
 
 export function jwt(data, routerHistory) {
   return (dispatch) => {
     dispatch({ type: 'LOADING' });
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/user_token'
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/user_token';
     return fetch(proxyUrl + targetUrl, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
+        Accept: 'application/json',
+        'Content-type': 'application/json',
       },
-      body: data
+      body: data,
     })
-    .then(response => {
-      if (response.status >= 200 && response.status < 300) {
-        response.json()
-        .then(data => {
-          localStorage.setItem('jwt', data.jwt)
-          dispatch({ type: 'RETURN_JWT'});
-          dispatch({ type: 'LOADING' });
-            var targetUrl1 = 'https://katy-hiking-trails.herokuapp.com/api/users/:id'
-          return fetch(proxyUrl + targetUrl1, {
-            method: 'GET',
-            headers: {
-              Authorization: 'Bearer ' + localStorage.getItem('jwt')
-            }
-          }).then(response => response.json())
-          .then(data => {
-            localStorage.setItem('user', JSON.stringify(data))
-            dispatch({ type: 'CURRENT_USER', payload: data });
-            routerHistory.replace('/');
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          response.json().then((data) => {
+            localStorage.setItem('jwt', data.jwt);
+            dispatch({ type: 'RETURN_JWT' });
+            dispatch({ type: 'LOADING' });
+            var targetUrl1 = 'https://katy-hiking-trails.fly.dev/api/users/:id';
+            return fetch(proxyUrl + targetUrl1, {
+              method: 'GET',
+              headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+              },
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                localStorage.setItem('user', JSON.stringify(data));
+                dispatch({ type: 'CURRENT_USER', payload: data });
+                routerHistory.replace('/');
+              });
           });
-        });
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    })
-    .catch(function(error) {
-      console.log('There has been a problem with your fetch operation: ', error.message);
-      dispatch({ type: 'INVALID_SIGNIN' })
-    });
-  }
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .catch(function (error) {
+        console.log(
+          'There has been a problem with your fetch operation: ',
+          error.message
+        );
+        dispatch({ type: 'INVALID_SIGNIN' });
+      });
+  };
 }
 
 export function signUp(data, routerHistory) {
   return (dispatch) => {
     dispatch({ type: 'LOADING' });
-    var proxyUrl = 'https://hidden-reaches-61697.herokuapp.com/',
-      targetUrl = 'https://katy-hiking-trails.herokuapp.com/api/register'
+    var proxyUrl = '',
+      targetUrl = 'https://katy-hiking-trails.fly.dev/api/register';
     return fetch(proxyUrl + targetUrl, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
+        Accept: 'application/json',
+        'Content-type': 'application/json',
       },
-      body: data
+      body: data,
     })
-    .then(response => {
-      if (response.status >= 200 && response.status < 300) {
-       response.json()
-        .then(data => {
-          localStorage.setItem('jwt', data.jwt)
-          dispatch({ type: 'RETURN_JWT' });
-          dispatch({ type: 'LOADING' });
-            var targetUrl1 = 'https://katy-hiking-trails.herokuapp.com/api/users/:id'
-          return fetch(proxyUrl + targetUrl1, {
-            method: 'GET',
-            headers: {
-              Authorization: 'Bearer ' + localStorage.getItem('jwt')
-            }
-          })
-          .then(response => response.json())
-          .then(data => {
-            localStorage.setItem('user', JSON.stringify(data))
-            dispatch({ type: 'CURRENT_USER', payload: data });
-            routerHistory.replace('/');
-          })
-        })
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    })
-    .catch(function(error) {
-      console.log('There has been a problem with your fetch operation: ', error.message);
-      dispatch({ type: 'INVALID_SIGNUP' })
-    });
-  }
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          response.json().then((data) => {
+            localStorage.setItem('jwt', data.jwt);
+            dispatch({ type: 'RETURN_JWT' });
+            dispatch({ type: 'LOADING' });
+            var targetUrl1 = 'https://katy-hiking-trails.fly.dev/api/users/:id';
+            return fetch(proxyUrl + targetUrl1, {
+              method: 'GET',
+              headers: {
+                Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+              },
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                localStorage.setItem('user', JSON.stringify(data));
+                dispatch({ type: 'CURRENT_USER', payload: data });
+                routerHistory.replace('/');
+              });
+          });
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .catch(function (error) {
+        console.log(
+          'There has been a problem with your fetch operation: ',
+          error.message
+        );
+        dispatch({ type: 'INVALID_SIGNUP' });
+      });
+  };
 }
 
 export function signOut() {
   return (dispatch) => {
-    dispatch({ type: 'LOGGED_OUT' })
+    dispatch({ type: 'LOGGED_OUT' });
     window.location.replace('/login');
-  }
+  };
 }
